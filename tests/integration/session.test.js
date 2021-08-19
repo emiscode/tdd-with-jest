c0dc377b36d1a30b33ff45d4c52476123d013dab
Emilio Scodeler <emilios@ciandt.com>
2021-08-19 17:27:33 -0300

    Create unit test for encrypt user pass

--- tests/integration/session.test.js
+++ tests/integration/session.test.js
@@ -1,20 +1,25 @@
 const request = require('supertest');
 const app = require('../../src/app');
 const { User } = require('../../src/app/models');
+const truncate = require('../utils/truncate');
 
 describe('Authentication', () => {
+    beforeEach(async () => {
+        await truncate();
+    });
+    
     it('should authenticate with valid credentials', async () => {
         const user = await User.create({
             name: 'emilio',
             email: 'emiscode@gmail.com',
-            password_hash: '123'
+            password: '123'
         });
 
         const response = await request(app)
-        .post('sessions')
+        .post('/sessions')
         .send({
             email: user.email,
-            password: '333'
+            password: user.password
         });
 
         expect(response.status).toBe(200);
